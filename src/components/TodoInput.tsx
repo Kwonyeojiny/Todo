e572b290f2937d6cf57f1cd6b36e71994d3877bd
Kwonyeojiny <kangagi0009@naver.com>
2025-07-10 13:19:44 +0900

    docs: useEffect, handleChange의 사용 의도에 대한 주석 추가

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -3,10 +3,14 @@ import { useEffect, useState } from "react";
 const TodoInput = () => {
   const [inputValue, setInputValue] = useState<string>("");
 
+  // 입력값 바뀌자마자 뭔가 하고 싶다 -> handleChange
+  // 상태가 바뀐 이후 효과를 실행하고 싶다 -> useEffect
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
+  // useEffect 사용법을 알기위한 코드
+  //  -> console.log와 같은 코드는 handleChange 함수 안에 넣는 것이 일반적
   useEffect(() => {
     console.log("변경된 값: ", inputValue);
   }, [inputValue]);
