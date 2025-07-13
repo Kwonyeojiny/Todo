18b98ba8779132d313c6d0cb23b049550d64aaae
Kwonyeojiny <kangagi0009@naver.com>
2025-07-13 17:16:37 +0900

    comment: TodoList 주석 제거

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,15 +11,6 @@ const TodoList = () => {
     );
     setTodos(updatedTodos);
     console.log("투두 업데이트: ", updatedTodos);
-
-    // NOTE: map을 사용하는 방식이 더 효율적
-    // map + 불변성(상태 변경 정확하게 감지) 유지
-    //   -> React 철학에 더 적합하고, 버그 발생 가능성 감소
-
-    // const newArr = [...todos];
-    // newArr[id].isComplete = !newArr[id].isComplete;
-    // setTodos(newArr);
-    // console.log("투두 업데이트: ", todos);
   };
 
   return (
