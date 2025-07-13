bff216f0b7640b7ce15e427537fbdf695d62de07
Kwonyeojiny <kangagi0009@naver.com>
2025-07-13 16:46:50 +0900

    test: 투두 mock 데이터 추가

---
+++ src/mock/todoList.ts
@@ -0,0 +1,7 @@
+import type { TodoInputProps } from "../types/todo";
+
+export const todoList: TodoInputProps[] = [
+  { id: 0, todo: "밥먹기", isComplete: false },
+  { id: 1, todo: "공부하기", isComplete: true },
+  { id: 2, todo: "청소하기", isComplete: false },
+];
